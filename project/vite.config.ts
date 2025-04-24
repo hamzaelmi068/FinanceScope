ce2611fdc86f458d1b8f6d26e4c3b2d2ab77770d
helmi <hamzaelmi067@gmail.com>
2025-04-23 23:33:36 -0400

    minor fix

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// vite.config.ts
 export default defineConfig({
-  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
